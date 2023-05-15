4ee7455e01dbb9da3e1d0d07f97debf5349a2414
Daniel Jorge <djpgonzaga@gmail.com>
2023-05-15 01:09:18 -0300

    feat: update dependencies, add more config files and better setup tailwind

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,6 +2,12 @@ module.exports = {
   extends: ["next", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve("next/babel")],
+    },
   },
 };
+
+// "react/jsx-key": "off",
