adf1d36009ad6ec6854f2ad0dad2434979e7c4d2
Daniel Jorge <djpgonzaga@gmail.com>
2023-05-16 01:25:21 -0300

    feat: display heading variations

--- apps/site/src/app/layout.tsx
+++ apps/site/src/app/layout.tsx
@@ -1,13 +1,21 @@
+import type { Metadata } from "next";
 import "ui/styles.css";
 
-export default function RootLayout({
-  children,
-}: {
+// @TODO: define metadata
+export const metadata: Metadata = {
+  title: "Web",
+};
+
+type RootLayoutProps = {
   children: React.ReactNode;
-}) {
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className="scroll-smooth">
+      <body className="pb-safe-bottom pl-safe-left pr-safe-right pt-safe-top antialiased">
+        {children}
+      </body>
     </html>
   );
 }
